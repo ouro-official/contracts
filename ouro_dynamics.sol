d3a35383f92927269d78365247e7a30039256422
ouro-finance <contact@ouro.finance>
2021-05-01 22:04:30 +0800

    add multi-collateral

--- ouro_dynamics.sol
+++ ouro_dynamics.sol
@@ -196,9 +196,9 @@ contract OURODynamics {
     IOGSToken public ogsContract = IOGSToken(0xEe5bCf20a21e0539Da126d8c86531E7BeE25933F);
     IERC20 public usdtContract = IERC20(0x55d398326f99059fF775485246999027B3197955);
 
-    AggregatorV3Interface public priceFeedBNB; // chainlink price feed
+    uint256 public OURO_PRICE_UNIT = 1e18;
 
-    uint constant internal MAX_SWAP_LATENCY = 60; // 1 minutes
+    uint constant internal MAX_SWAP_LATENCY = 1; // swap should be done immediately
     uint256 constant internal MAX_UINT256 = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
     
     // 1. The system will only mint new OGS and sell them for collateral when the value of the 
@@ -207,82 +207,175 @@ contract OURODynamics {
     //    burn when the value of the assets held in the pool is 3% higher than the value of the issued OURO
     uint public threshold = 3;
     
+    // CollateralInfo
+    struct CollateralInfo {
+        string Name;
+        IERC20 token;
+        bool isNative; // native asset
+        uint256 priceUnit; // usually 1e18
+        AggregatorV3Interface priceFeed; // asset price feed for xxx/USDT
+        bool approvedToRouter;
+    }
+    
+    // registered collaterals for OURO
+    CollateralInfo [] public collaterals;
+    
     /**
      * @dev update is the stability dynamic for ouro
      */
     function update() external {
         
-        // get assets in OURO contract
-        uint256 totalBNB = address(ouroContract).balance;
-        
-        uint256 totalOURO = ouroContract.totalSupply();
-        
-        // compute value priced in USDT
-        uint256 ouroValue = getOUROPrice().mul(totalOURO);
-        uint256 collateralValue = getBNBPrice().mul(totalBNB);
-
-        // value adjustment        
-        if (collateralValue >= ouroValue.mul(100+threshold).div(100)) {
+        // get total collateral value(USDT)
+        uint256 totalCollateralValue;
+        for (uint i=0;i<collaterals.length;i++) {
+            CollateralInfo storage collateral = collaterals[i];
             
+            if (collateral.isNative) {
+                // native assets, such as:
+                // ETH on ethereum , BNB on binance smart chain
+                totalCollateralValue += getAssetPrice(collateral.priceFeed)
+                                        .mul(address(ouroContract).balance)
+                                        .div(collateral.priceUnit);
+            } else {
+                // ERC20 assets (tokens)
+                totalCollateralValue += getAssetPrice(collateral.priceFeed)
+                                        .mul(collateral.token.balanceOf(address(ouroContract)))
+                                        .div(collateral.priceUnit);
+            }
+        }
+        
+        // get total OURO value(USDT)
+        uint256 totalOUROValue = ouroContract.totalSupply()
+                                        .mul(ouroContract.getPrice())
+                                        .div(OURO_PRICE_UNIT);
+        
+        // compute values diff
+        uint256 valueDiff;
+        bool buyOGS;
+        if (totalCollateralValue >= totalOUROValue.mul(100+threshold).div(100)) {
             // collaterals has excessive value to OURO value, 
             // 70% of the extra collateral would be used to BUY BACK OGS on secondary markets 
             // and conduct a token burn
-            uint256 bnbToBuyOGS = collateralValue.sub(ouroValue)
-                                                    .mul(70).div(100)
-                                                    .div(getBNBPrice());
+            valueDiff = totalCollateralValue.sub(totalOUROValue)
+                                                        .mul(70).div(100);
+            buyOGS = true;
             
-            // transfer BNB from OURO contract to this contract temporarily.
-            ouroContract.acquireBNB(bnbToBuyOGS);
-            
-            // the path to swap OGS out
-            address[] memory path = new address[](2);
-            path[0] = router.WETH();
-            path[1] = address(ogsContract);
-            
-            // calc amount OGS that could be swapped out with given BNB
-            uint [] memory amounts = router.getAmountsOut(bnbToBuyOGS, path);
-            uint256 ogsAmountOut = amounts[1];
-
-            // swap OGS out to this contract
-            router.swapExactTokensForETH(bnbToBuyOGS, ogsAmountOut, path, address(this), block.timestamp.add(MAX_SWAP_LATENCY));
-            
-            // burn OGS
-            ogsContract.burn(ogsAmountOut);
-    
-        } else if (collateralValue <= ouroValue.mul(100-threshold).div(100)) {
-
-
+        } else if (totalCollateralValue <= totalOUROValue.mul(100-threshold).div(100)) {
             // collaterals has less value to OURO value, mint new OGS to buy assets
-            uint256 bnbToBuyBack = ouroValue.sub(collateralValue)
-                                                .div(getBNBPrice());
-                                             
-            // the path to swap BNB out
-            address[] memory path = new address[](2);
-            path[0] = address(ogsContract);
-            path[1] = router.WETH();
+            valueDiff = totalOUROValue.sub(totalCollateralValue);
+        }
+        
+        // if no value diff found, return here, nothing should happen!
+        if (valueDiff == 0 ){
+            return;
+        }
+        
+        // buyback operations in pro-rata basis
+        for (uint i=0;i<collaterals.length;i++) {
+            CollateralInfo storage collateral = collaterals[i];
             
-            // calc amount OGS required to swap out given BNB amount
-            uint [] memory amounts = router.getAmountsIn(bnbToBuyBack, path);
-            uint256 ogsRequired = amounts[0];
-                        
-            // mint OGS to this contract to buy back BNB                             
-            ogsContract.mint(address(this), ogsRequired);
-
-            // swap out BNB with OGS to OURO contract
-            router.swapTokensForExactETH(ogsRequired, bnbToBuyBack, path, address(ouroContract), block.timestamp.add(MAX_SWAP_LATENCY));
+            uint256 slotValue;
+            if (collateral.isNative) {
+                slotValue = getAssetPrice(collateral.priceFeed)
+                                        .mul(address(ouroContract).balance)
+                                        .div(collateral.priceUnit);
+                                    
+            } else {
+                slotValue = getAssetPrice(collateral.priceFeed)
+                                        .mul(collateral.token.balanceOf(address(ouroContract)))
+                                        .div(collateral.priceUnit);
+            }
+            
+            // calc pro-rata buy back value(in USDT) for this collateral
+            uint256 slotBuyBackValue = slotValue.mul(valueDiff)
+                                        .div(totalCollateralValue);
+                                
+            // execute buyback
+            if (buyOGS) {
+                buybackOGS(collateral, slotBuyBackValue);
+            } else {
+                buybackCollateral(collateral, slotBuyBackValue);
+            }
         }
     }
     
-    // get USDT price for 1 OURO (1e18)
-    function getOUROPrice() public view returns(uint256) {
-        return 1e18;
+    /**
+     * @dev buy back OGS with collateral
+     */
+    function buybackOGS(CollateralInfo storage assetInfo, uint256 slotValue) internal {
+        uint256 collateralToBuyOGS = slotValue
+                                        .mul(assetInfo.priceUnit)
+                                        .div(getAssetPrice(assetInfo.priceFeed));
+
+        // the path to swap OGS out
+        address[] memory path = new address[](2);
+        path[0] = address(assetInfo.token);
+        path[1] = address(ogsContract);
+        
+        // calc amount OGS that could be swapped out with given BNB
+        uint [] memory amounts = router.getAmountsOut(collateralToBuyOGS, path);
+        uint256 ogsAmountOut = amounts[1];
+        
+        if (assetInfo.isNative) {
+            // for native assets, call ouro contract to transfer ETH, BNB to THIS contract
+            ouroContract.acquireNative(collateralToBuyOGS);
+
+            // swap OGS out with native assets to THIS contract
+            payable(address(router)).functionCallWithValue(
+                abi.encodeWithSelector(router.swapExactETHForTokens.selector, ogsAmountOut, path, address(this), block.timestamp.add(MAX_SWAP_LATENCY)),
+                collateralToBuyOGS
+            );
+            
+        } else {
+            // for ERC20 assets, transfer the tokens from ouro contract to THIS contract
+            assetInfo.token.safeTransferFrom(address(ouroContract), address(this), collateralToBuyOGS);
+            
+            // swap OGS out to THIS contract
+            router.swapExactTokensForTokens(collateralToBuyOGS, ogsAmountOut, path, address(this), block.timestamp.add(MAX_SWAP_LATENCY));
+        }
+
+        // burn OGS
+        ogsContract.burn(ogsAmountOut);
     }
     
-    // get USDT price for 1 BNB (1e18)
-    function getBNBPrice() public view returns(uint256) {
-        (, int latestPrice, , , ) = priceFeedBNB.latestRoundData();
+    /**
+     * @dev buy back collateral with OGS
+     */
+    function buybackCollateral(CollateralInfo storage assetInfo, uint256 slotValue) internal {
+        uint256 collateralToBuyBack = slotValue
+                                        .mul(assetInfo.priceUnit)
+                                        .div(getAssetPrice(assetInfo.priceFeed));
+                                             
+        // the path to swap collateral out
+        address[] memory path = new address[](2);
+        path[0] = address(ogsContract);
+        path[1] = address(assetInfo.token);
+        
+        // calc amount OGS required to swap out given collateral
+        uint [] memory amounts = router.getAmountsIn(collateralToBuyBack, path);
+        uint256 ogsRequired = amounts[0];
+                    
+        // mint OGS to this contract to buy back BNB                             
+        ogsContract.mint(address(this), ogsRequired);
 
-        if (latestPrice > 0) { // assume USDT & BNB decimal = 18
+        if (assetInfo.isNative) {
+            // swap out native assets ETH, BNB with OGS to OURO contract
+            router.swapTokensForExactETH(ogsRequired, collateralToBuyBack, path, address(ouroContract), block.timestamp.add(MAX_SWAP_LATENCY));
+
+        } else {
+            // swap out tokens out to OURO contract
+            router.swapTokensForExactTokens(ogsRequired, collateralToBuyBack, path, address(ouroContract), block.timestamp.add(MAX_SWAP_LATENCY));
+        }
+    }
+    
+    /**
+     * @dev get asset price for 1 price unit
+     * @notice DO NOT div by ASSET PRICE HERE
+     */
+    function getAssetPrice(AggregatorV3Interface feed) public view returns(uint256) {
+        (, int latestPrice, , , ) = feed.latestRoundData();
+
+        if (latestPrice > 0) {
             return uint(latestPrice);
         }
         return 0;
